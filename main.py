09ab1e0941be5a412caca53f058f4c8230a7adad
Westpol <81779508+Westpol@users.noreply.github.com>
2023-12-14 19:54:26 +0100

    Basic functionality

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import json
 import pygame
 import tkinter
+import time
 
+Ipads = {"moddate": int(time.time()), "Ipads": {"IT18563": {"name": "Benno Schörmann", "Klasse": 13}, "IT18363": {"name": "Benno Schörmann", "Klasse": 13}}}
 
+Ipads["Ipads"].update({"IT16563": {"name": "Benno Schörmann", "Klasse": 13}})
+
+for i in Ipads["Ipads"]:
+    print(i)
+    print(Ipads["Ipads"][i])
+ 
