3dc797f9de3fc3b1ce848bb2f039c38559d68c2a
Westpol <81779508+Westpol@users.noreply.github.com>
2023-12-25 01:03:46 +0100

    getting info of existing Ipads and adding new moved to Frontend

--- prompt.py
+++ prompt.py
@@ -11,19 +11,23 @@ class Backend:
             self.Ipads = json.load(jsonfile)
         self.Ipads["opendate"] = int(time.time())
 
-    def addnew(self):
-        itnum: str = str(input("Scan Barcode: "))
-        version: str = str(input("Version: "))
-        surname: str = input("Surname: ")
-        name: str = input("Name: ")
-        classNum = input("Class: ")
-        subclass: str = input("Subclass: ")
-        Data = {itnum: {"moddate": int(time.time()), "version": version, "surname": surname, "name": name, "class": classNum, "subclass": subclass, 'repair': False, 'repairdate': 0, 'dosys': False, 'comments': ''}}
-        self.Ipads["Ipads"].update(Data)
+    def addnew(self, itnum: str, version: str, surname: str, name: str, class_num, subclass: str):
+        data = {itnum: {"moddate": int(time.time()), "version": version, "surname": surname, "name": name, "class": class_num, "subclass": subclass, 'repair': False, 'repairdate': 0, 'dosys': False, 'comments': ''}}
+        self.Ipads["Ipads"].update(data)
 
     def close(self):
         with open("Data/Ipads.json", "w") as file:
             json.dump(self.Ipads, file, indent=4)
+
+    def inlist(self, itnum: str):
+        if itnum in self.Ipads["Ipads"]:
+            return True
+        return False
+
+    def print_ipad(self, itnum: str):
+        if self.inlist(itnum):
+            return self.Ipads["Ipads"][itnum]
+        raise "given IT-number not in system, bofore requesting directly, please check with inlist()"
 
 
 class Frontend:
@@ -37,12 +41,26 @@ class Frontend:
     def dump(self):
         print(self.data)
 
-    def add(self):
-        self.backend.addnew()
+    def add(self, itnum: str):
+        version: str = str(input("Version: "))
+        surname: str = input("Surname: ")
+        name: str = input("Name: ")
+        classNum = input("Class: ")
+        subclass: str = input("Subclass: ")
+        self.backend.addnew(itnum, version, surname, name, classNum, subclass)
+
+    def get_num(self):
+        itnum: str = str(input("Scan Barcode: "))
+        if self.backend.inlist(itnum):
+            print(self.backend.print_ipad(itnum))
+        else:
+            if input("IT number not in system, want to add? [y/n]") == "y":
+                self.add(itnum)
+            else:
+                print("mkayyyy")
 
 
 if __name__ == "__main__":
     front = Frontend()
-    front.add()
-    front.dump()
+    front.get_num()
     front.close()
