32d7b894b28e4bd105b306b2f46427222f5a85ff
Westpol <81779508+Westpol@users.noreply.github.com>
2024-01-01 13:18:43 +0100

    getting main ready for tkinter implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-import PySimpleGUI as psg
+from tkinter import *
+from tkinter import ttk
 import time
 
 
@@ -35,49 +36,14 @@ class Backend:
 class Frontend:
     def __init__(self):
         self.backend = Backend()
-        self.welcome = [[psg.Text("Scan Barcode in this Textbox"), psg.Input(key="IT-num")],
-                        [psg.Button('Submit', visible=False, bind_return_key=True)]]
-        self.window = None
-        self.event, self.values = None, None
-
-    def begin(self):
-        self.window = psg.Window('Ipad-Verwaltung', self.welcome)
-        self.event, self.values = self.window.read()
-        print(self.values["IT-num"])
-        self.window.close()
-        self.window = psg.Window('Ipad-Verwaltung', self.welcome)
-        self.event, self.values = self.window.read()
-        print(self.values["IT-num"])
 
     def close(self):
         self.backend.close()
 
-    def add(self, itnum: str):
-        version: str = str(input("Version: "))
-        surname: str = input("Surname: ")
-        name: str = input("Name: ")
-        classNum = input("Class: ")
-        subclass: str = input("Subclass: ")
-        self.backend.addnew(itnum, version, surname, name, classNum, subclass)
-
-    def show(self, itnum: str):
-        print(self.backend.get_ipad(itnum))
-        showStats = [[],
-                     [],
-                     [],
-                     [],
-                     []]
-
-    def get_num(self):
-        welcome = self.welcome
-        self.window = psg.Window('Ipad-Verwaltung', welcome)
-        event, values = self.window.read()
-        self.window.close()
-        if self.backend.inlist(values["IT-num"]):
-            self.show(values["IT-num"])
+    def mainloop(self):
+        self.close()
 
 
 if __name__ == "__main__":
-    front = Frontend()
-    front.get_num()
-    front.close()
+    frontend = Frontend()
+    frontend.mainloop()
