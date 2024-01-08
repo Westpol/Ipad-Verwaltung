90cf3a18c2df35a5dba6fbab9d2fc0ba8bb27cd1
Benno <81779508+Westpol@users.noreply.github.com>
2024-01-08 13:31:10 +0100

    Added show_data, problem with save command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 import time
 
 ''' 
@@ -58,9 +59,14 @@ class Backend:
                     if self.keyData[i][0] != "":
                         temp_string += self.keyData[i][0]
             time_delta = self.keyData[i][1]
-        self.itnum = temp_string[::-1]
 
-        return self.itnum
+        self.itnum = temp_string[::-1]
+        self.keyData = []
+
+        if self.itnum != "":
+            return self.itnum
+        else:
+            return None
 
 
 class Frontend:
@@ -100,6 +106,63 @@ class Frontend:
         manual_search.pack(anchor="w", side="bottom")
         self.accept_scan = True
 
+    def show_data(self, infos: dict, itnum: str):
+        self.accept_scan = False
+        self.root.columnconfigure(0, weight=2)
+        self.root.columnconfigure((1, 2, 3), weight=1)
+
+        textfield = ScrolledText(self.root)
+        textfield.insert("1.0", str(infos["comments"]))
+        textfield.grid(row=1, column=0, rowspan=7, padx="20")
+        notes = tk.Label(self.root, text="Anmerkungen:", font=("Arial", 15))
+        notes.grid(row=0, column=0, sticky="w")
+
+        user = tk.Label(self.root, text="Besitzer:", font=("Arial", 15))
+        user.grid(row=0, column=1, columnspan=3, sticky="w")
+        name = tk.Label(self.root, text=str("Name:  " + infos["surname"] + " " + infos["name"]))
+        name.grid(row=1, column=1, columnspan=3, sticky="w")
+        if infos["subclass"] is not None:
+            classs = tk.Label(self.root, text=str("Klasse:  " + str(infos["class"]) + "." + str(infos["subclass"])))
+        else:
+            classs = tk.Label(self.root, text=str("Klasse:  " + str(infos["class"])))
+        classs.grid(row=2, column=1, columnspan=3, sticky="w")
+        teacher = tk.Label(self.root, text=str("Lehrer:  " + infos["teacher"]))
+        teacher.grid(row=3, column=1, columnspan=2, sticky="w")
+        besitzer_bearbeiten = tk.Button(self.root, text="Bearbeiten")
+        besitzer_bearbeiten.grid(row=3, column=3, sticky="w")
+
+        device = tk.Label(self.root, text="Gerät:", font=("Arial", 15))
+        device.grid(row=4, column=1, columnspan=3, sticky="w")
+
+        if infos["repair"] is not False:
+            status = tk.Label(self.root, text=str("Status:  In Reperatur"))
+        elif infos["dosys"] is not False:
+            status = tk.Label(self.root, text=str("Status:  Bei Dosys eingesendet"))
+        else:
+            status = tk.Label(self.root, text=str("Status:  Standard"))
+        status.grid(row=5, column=1, columnspan=3, sticky="w")
+
+        history = tk.Button(self.root, text="History")
+        history.grid(row=6, column=1, columnspan=3, sticky="w")
+
+        save_button = tk.Button(self.root, text="Save")
+        save_exit_button = tk.Button(self.root, text="Save + Exit")
+        exit_button = tk.Button(self.root, text="Exit", command=self.exit_show_data)
+        save_button.grid(row=8, column=1)
+        save_exit_button.grid(row=8, column=2)
+        exit_button.grid(row=8, column=3)
+
+    def exit_show_data(self):
+        self.clear()
+        self.welcome_screen()
+
+    def save_and_exit(self, textbox, itnum):
+        self.save_data(textbox, itnum)
+        self.exit_show_data()
+
+    def save_data(self, textbox, itnum):
+        self.backend.Ipads["Ipads"][itnum]["comments"] = textbox
+
     def clear(self):
         for widget in self.root.winfo_children():
             widget.destroy()
@@ -113,6 +176,7 @@ class Frontend:
                     self.clear()
                     self.dropdown_menu()
                     print(itnum)
+                    self.show_data(self.backend.get_ipad(itnum), itnum)
             else:
                 self.backend.keyData.append((event.char, time.time()))
 
